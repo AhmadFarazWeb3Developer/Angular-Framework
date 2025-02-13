8209f4c715877bf23c3103aa1fe715fd41e01d8a
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2025-02-13 11:33:14 +0500

    Angular Learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,25 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'firstApp';
+  title = 'My First Angular App';
+
+  constructor() {
+    console.log(this.title);
+  }
+
+  // Life Cycle hook like useEffect in react
+  ngOnInit() {
+    console.log('ng init called');
+    // this.changeTitle();
+  }
+
+  changeTitle() {
+    this.title = 'My First Angular App Changed';
+  }
 }
